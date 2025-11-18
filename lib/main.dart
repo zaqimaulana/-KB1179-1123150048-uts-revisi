23da4cfc5c4cbc3734c59c74ee9e562bb7cf284e
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-19 03:16:43 +0700

    menghubungkan main dart ke splash screen 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screen/splash_screen_1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
+      home: const SplashScreen1(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
 
   @override
   Widget build(BuildContext context) {
