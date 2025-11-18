7f4790c66b2dae054b9470cbb87758120c808bc7
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-19 04:09:57 +0700

    menambahkan gambar ke dalam lingkaran

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -9,12 +9,18 @@ class SplashScreen1 extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          const SizedBox(height: 50),
           Container(
             width: 250,
             height: 250,
             decoration: const BoxDecoration(
               shape: BoxShape.circle,
               color: Colors.green,
+              image: DecorationImage(
+                  image: AssetImage("assets/images/splash1.png"),
+                  fit: BoxFit.cover, 
+                  alignment: Alignment.center, 
+                ),
             ),
           ),
         ],
