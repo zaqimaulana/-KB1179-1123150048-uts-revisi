6e31859c1aca1408f8ea9e4f7480bc018324c431
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-19 03:06:12 +0700

    membuat page utama pada main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Nim : 1123150048, zaqi",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
